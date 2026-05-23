fe9a4ac1487176d8810ead7f856abe218b308198
LMLTech <leminhliem.it@gmail.com>
2026-05-23 18:29:44 +0700

    LMLTech - feat: configure CORS to allow cross-origin requests from frontend

--- fmp_backend/fitness-api/src/main/java/com/fitness/api/config/SecurityConfig.java
+++ fmp_backend/fitness-api/src/main/java/com/fitness/api/config/SecurityConfig.java
@@ -12,6 +12,12 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+// khai báo thư viện CORS để chuẩn bị kết nối với frontend
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import java.util.List;
+
 @Configuration // Khai báo class config cho Spring
 @EnableWebSecurity // Bật Spring Security
 @EnableMethodSecurity // kiểm tra quyền trên từng API
@@ -25,10 +31,30 @@ public class SecurityConfig {
         return new BCryptPasswordEncoder();
     }
 
+    // Cấu hình bean cors
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration configuration = new CorsConfiguration();
+        // Cho phép tất cả các domain Frontend (localhost:3000, localhost:5173...)
+        configuration.setAllowedOriginPatterns(List.of("*"));
+        // Cho phép các method HTTP
+        configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE", "OPTIONS", "PATCH"));
+        // Cho phép truyền các Header quan trọng (đặc biệt là Authorization chứa Token)
+        configuration.setAllowedHeaders(List.of("Authorization", "Content-Type", "X-Requested-With", "Accept"));
+        // Bắt buộc set true để Frontend có thể nhận và gửi token một cách an toàn
+        configuration.setAllowCredentials(true);
+
+        UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        // Áp dụng cấu hình CORS này cho toàn bộ API (/**)
+        source.registerCorsConfiguration("/**", configuration);
+        return source;
+    }
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
+                // cấu hình cors
+                .cors(cors -> cors.configurationSource(corsConfigurationSource()))
                 // Tắt CSRF vì đây là API
                 .csrf(AbstractHttpConfigurer::disable)
 
